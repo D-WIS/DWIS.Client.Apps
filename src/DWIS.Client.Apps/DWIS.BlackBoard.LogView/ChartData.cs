65314fc40582764ac0c87a65df8b7a02bc989d47
Benoît Daireaux <beda@norceresearch.no>
2026-02-14 15:05:28 +0100

    First draft of a log visualization app based on dwis bb

---
+++ src/DWIS.Client.Apps/DWIS.BlackBoard.LogView/ChartData.cs
@@ -0,0 +1,15 @@
+﻿namespace DWIS.BlackBoard.LogView
+{
+    public class ChartData
+    {
+        public string Prototype { get; set; }        
+        public double ConversionFactor { get; set; }
+        public string AxisName { get; set; }
+    }
+
+    public class AxisData
+    {
+        public string Name { get; set; }
+        public string Label { get; set; }
+    }
+}
---
+++ src/DWIS.Client.Apps/DWIS.BlackBoard.LogView/Program.cs
@@ -0,0 +1,38 @@
+using DWIS.BlackBoard.LogView.Components;
+using DWIS.Client.ReferenceImplementation;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
+builder.Services.AddSingleton<IOPCUADWISClient, DWIS.Client.ReferenceImplementation.OPCFoundation.DWISClientOPCF>();
+builder.Services.AddSingleton<IDWISClientConfiguration>(new DefaultDWISClientConfiguration());
+
+
+
+var app = builder.Build();
+
+var client = app.Services.GetRequiredService<IOPCUADWISClient>();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
